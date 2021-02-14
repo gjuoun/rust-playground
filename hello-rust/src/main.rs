020f6bb5754a6946e749d12e7545161548c991ae
John Guo <gjuoun@gmail.com>
2021-02-14 03:38:35 -0400

    first commit

---
+++ hello-rust/src/main.rs
@@ -0,0 +1,16 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+
+fn main() {
+    // let stdout = stdout();
+    // let message = String::from("hello fellow Rest");
+    // let width = message.chars().count();
+
+    // let mut writer = BufWriter::new(stdout.lock());
+    // say(message.as_bytes(), width, &mut writer).unwrap();
+
+    let x = 5;
+
+    let x = 0;
+    println!("the value of x is {}", x)
+}
