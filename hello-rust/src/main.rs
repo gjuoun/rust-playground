4eb33c5478a5a51b44502ed2c0e66a6f6d002f93
John Guo <gjuoun@gmail.com>
2021-02-14 05:24:57 -0400

    at range

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,16 +1,7 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
-
 fn main() {
-    // let stdout = stdout();
-    // let message = String::from("hello fellow Rest");
-    // let width = message.chars().count();
+  let a = [10, 20, 30, 40, 50];
 
-    // let mut writer = BufWriter::new(stdout.lock());
-    // say(message.as_bytes(), width, &mut writer).unwrap();
-
-    let x = 5;
-
-    let x = 0;
-    println!("the value of x is {}", x)
+  for number in (1..10).rev() {
+    println!("{}", number);
+  }
 }
