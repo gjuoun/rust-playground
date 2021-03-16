16464e0f6705125646f908da0ddfe5ee60e26cc8
John Guo <gjuoun@gmail.com>
2021-03-15 23:24:45 -0300

    starting 3.3 functions

---
+++ functions/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Rust code use `snake case` as the conventional style for cuntion and variable names
+    // snake caasdfe all letters are lowercase and underscores separate workds.
+    another_function();
+}
+
+
+fn another_function(){
+    println!("anotherfunction");
+}
