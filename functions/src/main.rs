13aeb641c627a88942a5f91f2fe527135cb4d983
John Guo <gjuoun@gmail.com>
2021-03-16 03:24:27 -0300

    more functions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,10 +1,22 @@
 fn main() {
     // Rust code use `snake case` as the conventional style for cuntion and variable names
     // snake caasdfe all letters are lowercase and underscores separate workds.
-    another_function();
+
+    
+    println!("{}",another_function(565));
+
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x+1
+    };
+
+    println!("The value of y is {}", y);
 }
 
 
-fn another_function(){
-    println!("anotherfunction");
+fn another_function(x: i32) -> i32 {
+    x
 }
+
