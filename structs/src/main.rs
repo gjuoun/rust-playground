567adec122316cd54467a59a9757b4c6166f5ff1
John Guo <gjuoun@gmail.com>
2021-03-23 01:12:53 -0300

    done structs

---
+++ structs/src/main.rs
@@ -0,0 +1,26 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 20,
+        ..rect1
+    };
+
+    println!("The area is {}", rect1.area());
+    println!("The area is {}", rect2.area());
+}
+
