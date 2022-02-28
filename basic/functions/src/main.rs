ab64e5488e671f5b35f98ef7e46e2ac701b78952
Jun Guo <gjuoun@gmail.com>
2022-02-28 11:39:13 -0400

    working on 3.2

--- basic/functions/src/main.rs
+++ basic/functions/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    // Rust code use `snake case` as the conventional style for cuntion and variable names
+    // Rust code use `snake case` as the conventional style for funtion and variable names
     // snake caasdfe all letters are lowercase and underscores separate workds.
 
     
@@ -9,7 +9,7 @@ fn main() {
 
     let y = {
         let x = 3;
-        x+1
+        x+3
     };
 
     println!("The value of y is {}", y);
