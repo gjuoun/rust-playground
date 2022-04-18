5175c1506d23c33d5c4429249e6b95c7a94b2a6d
Jun Guo <gjuoun@gmail.com>
2022-04-18 01:47:29 -0300

    play

--- book/_1cli/src/main.rs
+++ book/_1cli/src/main.rs
@@ -56,9 +56,9 @@ fn main() {
 }
 
 use regex::Regex;
-fn replace(target: &str, replacement: &str, text: &str) {
+fn replace(target: &str, replacement: &str, text: &str) -> Result<String, regex::Error> {
     let regex = Regex::new(target)?;
-    
+    Ok(regex.replace_all(text, replacement).to_string())
 }
 
 use std::env;
