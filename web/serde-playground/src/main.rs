0360a2a77ac4dcc7f91410c91825d788cc42c1c4
Jun Guo <gjuoun@gmail.com>
2022-10-03 00:52:39 -0300

    done json playing

--- web/serde-playground/src/main.rs
+++ web/serde-playground/src/main.rs
@@ -1,22 +1,52 @@
-use serde::{ Serialize, Deserialize};
-use serde_json::{ json};
-use std::fs;
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+use reqwest::Url;
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Person {
-    name: String,
-    age: u8,
-    phones: Vec<String>,
+#[serde(rename_all = "camelCase")]
+struct Todo {
+    user_id: u32,
+    id: u32,
+    title: String,
+    completed: bool,
 }
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let resp = reqwest::get("https://jsonplaceholder.typicode.com/todos/1")
+    //     .await?
+    //     .json::<Todo>()
+    //     .await?;
+    // let client = reqwest::Client::new();
 
-fn main() {
-    let person = Person {
-        name: "John Doe".to_owned(),
-        age: 43,
-        phones: vec!["+44 1234567".to_owned(), "+44 2345678".to_owned()],
-    };
+    // let resp = client
+    //     .post("https://jsonplaceholder.typicode.com/todos")
+    //     .json(&Todo {
+    //         user_id: 1,
+    //         id: 1,``
+    //         title: "test".to_owned(),
+    //         completed: false,
+    //     })
+    //     .send()
+    //     .await?;
+    // // println!("{:#?}", resp.json().await?);
 
-    println!("{}", serde_json::to_string(&person).unwrap());
+    // let content_type = resp
+    //     .headers()
+    //     .get("content-type")
+    //     .ok_or("missing content-type header")?
+    //     .to_str()?;
+    // println!("{}", content_type);
 
+    // let return_todo = resp.json::<Todo>().await?;
+    // println!("{:#?}", return_todo);
+
+    let url = Url::parse("https://example.com/products?page=2&sort=desc")?;
+    let pairs: HashMap<_,_> = url.query_pairs().into_owned().collect();
+
+    println!("{}", pairs.get("page").ok_or("no page")?);
+    println!("{}", pairs.get("sort").ok_or("no sort")?);
+
+    Ok(())
 }
