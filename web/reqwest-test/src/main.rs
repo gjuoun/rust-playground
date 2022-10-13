97712cf207c21e9726226471822a8a740fe3a580
Jun Guo <gjuoun@gmail.com>
2022-10-12 21:37:42 -0300

    optional body

--- web/reqwest-test/src/main.rs
+++ web/reqwest-test/src/main.rs
@@ -3,19 +3,23 @@ use serde_json::{json, Value};
 use std::collections::HashMap;
 
 #[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 struct Post {
     id: Option<u32>,
     title: String,
-    body: String,
+    body: Option<String>,
     user_id: u32,
+    completed: Option<bool>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let resp = reqwest::get("https://jsonplaceholder.typicode.com/todos/1")
-    //     .await?
-    //     .json::<HashMap<String, Value>>()
-    //     .await?;
+    let resp = reqwest::get("https://jsonplaceholder.typicode.com/todos/1")
+        .await?
+        .json::<Post>()
+        .await?;
+
+    println!("{:#?}", resp);
 
     let client = reqwest::Client::new();
     let resp = client
@@ -23,8 +27,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .json(&Post {
             id: None,
             title: "foo".to_owned(),
-            body: "bar".to_owned(),
+            body: Some("bar".to_owned()),
             user_id: 1,
+            completed: Some(true),
         })
         .send()
         .await?;
