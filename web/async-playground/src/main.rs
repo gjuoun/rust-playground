2013d98bea9efb581951b63f008df7becc160afe
Jun Guo <gjuoun@gmail.com>
2022-10-17 01:17:47 -0300

    deep dive in trait

--- web/async-playground/src/main.rs
+++ web/async-playground/src/main.rs
@@ -1,33 +1,25 @@
-use std::ops::Add;
 
-// 为Point结构体派生Debug特征，用于格式化输出
-#[derive(Debug)]
-struct Point<T: Add<T, Output = T>> { //限制类型T必须实现了Add特征，否则无法进行+操作。
-    x: T,
-    y: T,
+// 使用至少两种方法让代码工作
+// 不要添加/删除任何代码行
+trait MyTrait {
+  fn f(&self) -> Self;
 }
 
-impl<T: Add<T, Output = T>> Add for Point<T> {
-    type Output = Point<T>;
-
-    fn add(self, p: Point<T>) -> Point<T> {
-        Point{
-            x: self.x + p.x,
-            y: self.y + p.y,
-        }
-    }
+impl MyTrait for u32 {
+  fn f(&self) -> u32 { 42 }
 }
 
-fn add<T: Add<T, Output=T>>(a:T, b:T) -> T {
-    a + b
+impl MyTrait for String {
+  fn f(&self) -> String { self.clone() }
+}
+
+fn my_function(x: impl MyTrait) -> impl MyTrait {
+  x.f()
 }
 
 fn main() {
-    let p1 = Point{x: 1.1f32, y: 1.1f32};
-    let p2 = Point{x: 2.1f32, y: 2.1f32};
-    println!("{:?}", add(p1, p2));
+  my_function(13_u32);
+  my_function(String::from("abc"));
 
-    let p3 = Point{x: 1i32, y: 1i32};
-    let p4 = Point{x: 2i32, y: 2i32};
-    println!("{:?}", add(p3, p4));
+  println!("Success!")
 }
