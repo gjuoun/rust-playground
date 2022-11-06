2539fc4c5db36631ee710a5333594104ec21583f
gjuoun <gjuoun@gmail.com>
2022-11-06 18:22:00 -0400

    iterators

--- advanced/defer/src/main.rs
+++ advanced/defer/src/main.rs
@@ -1,24 +1,39 @@
-
-struct MyBox<T>(T);
+struct MyBox<T> {
+    v: T,
+}
 
 impl<T> MyBox<T> {
     fn new(x: T) -> MyBox<T> {
-        MyBox(x)
+        MyBox { v: x }
     }
 }
 
-
 use std::ops::Deref;
+
+// implement Deref for MyBox
 impl<T> Deref for MyBox<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.v
+    }
+}
+
+use std::ops::DerefMut;
+
+// implement DerefMut for MyBox,it needs Deref first
+impl<T> DerefMut for MyBox<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.v
     }
 }
 
 fn main() {
-    
-    let x = MyBox::new(1);
-    assert_eq!(1, *x);
+    let mut s = MyBox::new(String::from("hello, "));
+    display(&mut s)
+}
+
+fn display(s: &mut String) {
+    s.push_str("world");
+    println!("{}", s);
 }
