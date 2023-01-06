d9bd67e858832a62eca88b8fa068cd1e308531d7
Jun Guo <gjuoun@gmail.com>
2023-01-06 02:40:00 -0400

    working for myself

--- web/hyper-playground/src/main.rs
+++ web/hyper-playground/src/main.rs
@@ -1,69 +1,45 @@
-use futures::TryStreamExt as _;
+#![deny(warnings)]
+// use futures::io::Empty;
+// use futures::TryStreamExt as _;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
-use std::convert::Infallible;
-use std::net::SocketAddr;
 
+/// This is our service handler. It receives a Request, routes on its
+/// path, and returns a Future of a Response.
 async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    // let method = req.method();
+    // let path = req.uri().path();
     let mut response = Response::new(Body::empty());
 
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") => {
-            *response.body_mut() = Body::from("Try POSTing data to /echo");
+        (&Method::GET, "/") =>  {
+            *response.body_mut() = "Something is good".into();
         }
         (&Method::POST, "/echo") => {
-            // we'll be back
             *response.body_mut() = req.into_body();
         }
-        // Yet another route inside our match block...
-        (&Method::POST, "/echo/uppercase") => {
-            // This is actually a new `futures::Stream`...
-            let mapping = req.into_body().map_ok(|chunk| {
-                chunk
-                    .iter()
-                    .map(|byte| byte.to_ascii_uppercase())
-                    .collect::<Vec<u8>>()
-            });
 
-            // Use `Body::wrap_stream` to convert it to a `Body`...
-            *response.body_mut() = Body::wrap_stream(mapping);
-        },
-        (&Method::POST, "/echo/reverse") => {
-            // Await the full body to be concatenated into a single `Bytes`...
-            let full_body = hyper::body::to_bytes(req.into_body()).await?;
-        
-            // Iterate the full body in reverse order and collect into a new Vec.
-            let reversed = full_body.iter()
-                .rev()
-                .cloned()
-                .collect::<Vec<u8>>();
-        
-            *response.body_mut() = reversed.into();
-        },
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
         }
-    };
+    }
+    
+
 
     Ok(response)
 }
 
 #[tokio::main]
-async fn main() {
-    // We'll bind to 127.0.0.1:3000
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = ([127, 0, 0, 1], 3000).into();
 
-    // A `Service` is needed for every connection, so this
-    // creates one from our `hello_world` function.
-    let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn(echo))
-    });
+    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(echo)) });
 
-    let server = Server::bind(&addr).serve(make_svc);
+    let server = Server::bind(&addr).serve(service);
 
-    // Run this server for... forever!
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
-    }
+    println!("Listening on http://{}", addr);
+
+    server.await?;
+
+    Ok(())
 }
