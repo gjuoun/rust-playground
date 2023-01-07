a94cd994bfea61aef162fd5ec6b5a30726754693
Jun Guo <gjuoun@gmail.com>
2023-01-07 00:47:24 -0400

    write a client

--- web/hyper-playground/src/main.rs
+++ web/hyper-playground/src/main.rs
@@ -1,45 +1,30 @@
-#![deny(warnings)]
-// use futures::io::Empty;
-// use futures::TryStreamExt as _;
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use hyper::{Client, Request, Method, Body};
+use hyper::body::HttpBody as _;
+use tokio::io::{stdout, AsyncWriteExt as _};
+use serde::{Deserialize, Serialize};
 
-/// This is our service handler. It receives a Request, routes on its
-/// path, and returns a Future of a Response.
-async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    // let method = req.method();
-    // let path = req.uri().path();
-    let mut response = Response::new(Body::empty());
-
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") =>  {
-            *response.body_mut() = "Something is good".into();
-        }
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = req.into_body();
-        }
-
-        _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
-        }
-    }
-    
-
-
-    Ok(response)
+#[derive(Serialize, Deserialize, Debug)]
+struct Data {
+    origin: String,
+}
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
 }
 
+
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = ([127, 0, 0, 1], 3000).into();
+async fn main()-> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let req = Request::builder()
+    .method(Method::POST)
+    .uri("http://httpbin.org/post")
+    .header("content-type", "application/json")
+    .body(Body::from(r#"{"library":"hyper"}"#))?;
 
-    let service = make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(echo)) });
+// We'll send it in a second...
 
-    let server = Server::bind(&addr).serve(service);
-
-    println!("Listening on http://{}", addr);
-
-    server.await?;
 
     Ok(())
 }
+
