aa6e4fcea680d9bc21d02a2934b8139397c12372
Jun Guo <gjuoun@gmail.com>
2023-02-04 17:34:21 -0400

    good use

--- web/hello_salvo/src/router/game.rs
+++ web/hello_salvo/src/router/game.rs
@@ -1,8 +1,8 @@
-use salvo::{handler, Request, Router};
-use serde::Deserialize;
+use salvo::{handler, prelude::Extractible, writer::Json, Request, Response, Router};
+use serde::{Deserialize, Serialize};
 
 pub fn game_router() -> Router {
-    Router::with_path("/game/<id>").get(game).post(game_details)
+    Router::with_path("/game/").push(Router::with_path("<id>").get(game).post(game_details))
 }
 
 #[handler]
@@ -18,8 +18,28 @@ struct GameDetails {
     title: String,
 }
 
+#[derive(Serialize, Deserialize, Extractible, Debug, Clone, Copy)]
+/// Get the data field value from the body by default.
+#[extract(default_source(from = "body", format = "json"))]
+struct GoodMan<'a> {
+    /// The id number is obtained from the request path parameter, and the data is automatically parsed as i64 type.
+    #[extract(source(from = "param"))]
+    id: i64,
+    /// Reference types can be used to avoid memory copying.
+    username: &'a str,
+}
+
+// #[handler]
+// async fn game_details(req: &mut Request) -> String {
+//     let good_man: GoodMan<'_> = req.extract().await.unwrap();
+
+//     format!(
+//         "Good man is id: {}, name: {}",
+//         good_man.id, good_man.username
+//     )
+// }
+
 #[handler]
-async fn game_details(req: &mut Request) -> String {
-    let other_game = req.parse_json::<GameDetails>().await.unwrap();
-    return format!("Game, {}", other_game.title);
+async fn game_details<'a>(good_man: GoodMan<'a>, res: &mut Response) {
+    res.render(Json(good_man))
 }
