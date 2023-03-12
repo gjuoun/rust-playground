3404eb973a22d41fc5308c77557b143c4d4186af
juncurbie <jun@curbie.ca>
2023-03-11 23:01:18 -0400

    plugin work properly

--- bevy/my_bevy_game/src/main.rs
+++ bevy/my_bevy_game/src/main.rs
@@ -1,43 +1,10 @@
 use bevy::prelude::*;
 
-// use my_bevy_game::plugin::HelloPlugin;
-
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        // add things to your app here
-        // app.add_startup_system(add_people)
-        //     .add_system(hello_world)
-        //     .add_system(greet_people);
-    }
-}
-
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn hello_world() {
-    println!("hello world!");
-}
-
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
-    }
-}
+use my_bevy_game::plugin::HelloPlugin;
 
 fn main() {
     App::new()
-        // .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
         .run();
 }
--- bevy/my_bevy_game/src/plugin/mod.rs
+++ bevy/my_bevy_game/src/plugin/mod.rs
@@ -11,9 +11,9 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         // add things to your app here
-        // app.add_startup_system(add_people)
-        //     .add_system(hello_world)
-        //     .add_system(greet_people);
+        app.add_startup_system(add_people)
+            .add_system(hello_world)
+            .add_system(greet_people);
     }
 }
 
