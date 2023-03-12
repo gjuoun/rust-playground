fcf316e789c1c678a99b62872d0fe957a0e5b2b3
juncurbie <jun@curbie.ca>
2023-03-12 00:05:14 -0400

    add timer

--- bevy/my_bevy_game/src/plugin/mod.rs
+++ bevy/my_bevy_game/src/plugin/mod.rs
@@ -1,4 +1,11 @@
-use bevy::prelude::{App, Commands, Component, Plugin, Query, With};
+mod timer_res;
+
+use bevy::{
+    prelude::{App, Commands, Component, Plugin, Query, With, Res, ResMut},
+    time::{Timer, TimerMode, Time},
+};
+
+use self::timer_res::GreetTimer;
 
 #[derive(Component)]
 struct Person;
@@ -11,8 +18,8 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
         // add things to your app here
-        app.add_startup_system(add_people)
-            .add_system(hello_world)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
             .add_system(greet_people);
     }
 }
@@ -23,12 +30,14 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn hello_world() {
-    println!("hello world!");
-}
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    let inner_timer = &mut timer.0;
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+    if inner_timer.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
---
+++ bevy/my_bevy_game/src/plugin/timer_res.rs
@@ -0,0 +1,11 @@
+use bevy::{
+    prelude::{Query, Res, ResMut, Resource, With},
+    time::{Time, Timer},
+};
+
+use super::{Name, Person};
+
+#[derive(Resource)]
+pub struct GreetTimer(pub Timer);
+
+
